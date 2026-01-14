cd6da94288fd53a9c50d8fe1a60614ff293d8c24
Alvaro <alvaro.sarria.rico@gmail.com>
2026-01-14 14:08:14 +0100

    adding generic user for monbodb DB

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it.
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, (req, res) => {
   console.log(`Server listening on port: ${PORT}`);
 });
