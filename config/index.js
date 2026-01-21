200043b36cb4749956324809f877c09a947a78df
Alvaro <alvaro.sarria.rico@gmail.com>
2026-01-21 18:57:13 +0100

    testing cors

--- config/index.js
+++ config/index.js
@@ -20,11 +20,11 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  // app.use(
-  //   cors({
-  //     origin: "*",
-  //   }),
-  // );
+  app.use(
+    cors({
+      origin: "https://app-fair-share.vercel.app",
+    }),
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
